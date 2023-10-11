49359e6470faabc06c86d242ae671466abfb9d7c
GHABRI Abdallah <abdallah.ghabri@sofrecom.com>
2023-10-11 16:11:03 +0100

    stockage des arguments dans une liste

--- test.php
+++ test.php
@@ -4,19 +4,32 @@ if (php_sapi_name() != 'cli') {
     exit("Ce script ne peut être exécuté qu'en ligne de commande.\n");
 }
 
-foreach ($argv as $arg) {
+$argType = null;
+$values = [];
 
-    if ($arg) {
-        printf("argument existe.\n");
-    }
-    if ($arg === '-g') {
-        return($foo[1]);
-        exit("Il s'agit d'une liste de chaînes.\n");
-    } elseif ($arg === '-d') {
-        exit("Il s'agit d'une liste d'entiers.\n");
-    } elseif ($arg === '-b') {
-        exit("Il s'agit d'un booléen (TRUE).\n");
+foreach ($argv as $arg) {
+    if ($arg === '-g' || $arg === '-d' || $arg === '-b') {
+        $argType = $arg;
     } else {
-        printf("Argument inconnu : %s\n", $arg);
+        if ($argType) {
+            $values[] = $arg;
+        } else {
+            printf("Argument inconnu : %s\n", $arg);
+        }
     }
 }
+
+if ($argType === '-g') {
+    $result = implode(' ', $values);
+    printf("Il s'agit d'une liste de chaînes : %s\n", $result);
+} elseif($argType === '-abdallah'){
+    printf("entrez votre mdp");
+} elseif ($argType === '-d') {
+    $result = array_sum($values);
+    printf("Il s'agit d'une liste d'entiers, somme : %d\n", $result);
+} elseif ($argType === '-b') {
+    printf("Il s'agit d'un booléen (TRUE).\n");
+} else {
+    printf("Aucun argument valide spécifié.\n");
+}
+
